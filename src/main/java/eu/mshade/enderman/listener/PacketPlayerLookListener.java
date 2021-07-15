b8b256481e57aa40e7b07224b3127a596d1523f0
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-15 22:40:17 +0200

    add PacketPlayerLookListener 🎠

---
+++ src/main/java/eu/mshade/enderman/listener/PacketPlayerLookListener.java
@@ -0,0 +1,22 @@
+package eu.mshade.enderman.listener;
+
+import eu.mshade.enderframe.EnderFrame;
+import eu.mshade.enderframe.event.entity.PacketMoveEvent;
+import eu.mshade.enderframe.event.entity.PacketMoveType;
+import eu.mshade.enderframe.world.Position;
+import eu.mshade.enderman.packet.play.PacketInPlayerLook;
+import eu.mshade.mwork.event.EventContainer;
+import eu.mshade.mwork.event.EventListener;
+
+public class PacketPlayerLookListener implements EventListener<PacketInPlayerLook> {
+
+    @Override
+    public void onEvent(PacketInPlayerLook event, EventContainer eventContainer) {
+        EnderFrame.get().getPacketEventBus().publish(
+                new PacketMoveEvent(
+                        new Position(event.getYaw(), event.getPith(), event.isOnGround()),
+                        PacketMoveType.LOOK),
+                eventContainer);
+    }
+
+}
