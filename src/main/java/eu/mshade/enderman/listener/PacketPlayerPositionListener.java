1dba560b20dab53907481849155cd3eda4cebcb8
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-16 23:21:04 +0200

    add PacketPlayerPositionListener 🎭

---
+++ src/main/java/eu/mshade/enderman/listener/PacketPlayerPositionListener.java
@@ -0,0 +1,22 @@
+package eu.mshade.enderman.listener;
+
+import eu.mshade.enderframe.EnderFrame;
+import eu.mshade.enderframe.event.entity.PacketMoveEvent;
+import eu.mshade.enderframe.event.entity.PacketMoveType;
+import eu.mshade.enderframe.world.Position;
+import eu.mshade.enderman.packet.play.PacketInPlayerPosition;
+import eu.mshade.mwork.event.EventContainer;
+import eu.mshade.mwork.event.EventListener;
+
+public class PacketPlayerPositionListener implements EventListener<PacketInPlayerPosition> {
+
+    @Override
+    public void onEvent(PacketInPlayerPosition event, EventContainer eventContainer) {
+        EnderFrame.get().getPacketEventBus().publish(
+                new PacketMoveEvent(
+                        new Position(event.getX(), event.getY(), event.getZ(), event.isOnGround()),
+                        PacketMoveType.POSITION),
+                eventContainer);
+    }
+
+}
