2fc3b5a86d6443e34614e2c1ea43436dbadada9e
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 20:12:17 +0200

    add PacketOutJoinGame 🎨

---
+++ src/main/java/eu/mshade/enderman/packet/play/PacketOutJoinGame.java
@@ -0,0 +1,38 @@
+package eu.mshade.enderman.packet.play;
+
+import eu.mshade.enderframe.GameMode;
+import eu.mshade.enderframe.protocol.ByteMessage;
+import eu.mshade.enderframe.protocol.PacketOut;
+import eu.mshade.enderframe.world.Difficulty;
+import eu.mshade.enderframe.world.Dimension;
+
+public class PacketOutJoinGame extends PacketOut {
+
+    private final GameMode gameMode;
+    private final Dimension dimension;
+    private final Difficulty difficulty;
+    private final int maxPlayers;
+    private final String levelType;
+    private final boolean reducedDebugInfo;
+
+    public PacketOutJoinGame(GameMode gameMode, Dimension dimension, Difficulty difficulty, int maxPlayers, String levelType, boolean reducedDebugInfo) {
+        this.gameMode = gameMode;
+        this.dimension = dimension;
+        this.difficulty = difficulty;
+        this.maxPlayers = maxPlayers;
+        this.levelType = levelType;
+        this.reducedDebugInfo = reducedDebugInfo;
+    }
+
+    @Override
+    public void serialize(ByteMessage byteMessage) {
+        byteMessage.writeInt(3);
+        byteMessage.writeByte(gameMode.getId());
+        byteMessage.writeByte(dimension.getId());
+        byteMessage.writeByte(difficulty.getId());
+        byteMessage.writeByte(maxPlayers);
+        byteMessage.writeString(levelType);
+        byteMessage.writeBoolean(reducedDebugInfo);
+    }
+
+}
