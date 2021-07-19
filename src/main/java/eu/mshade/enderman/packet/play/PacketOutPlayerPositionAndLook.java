1b3077176966c6c6970d632d3fe259c49325c3a8
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 20:11:39 +0200

    add PacketOutPlayerPositionAndLook 🧵

---
+++ src/main/java/eu/mshade/enderman/packet/play/PacketOutPlayerPositionAndLook.java
@@ -0,0 +1,31 @@
+package eu.mshade.enderman.packet.play;
+
+import eu.mshade.enderframe.protocol.ByteMessage;
+import eu.mshade.enderframe.protocol.PacketOut;
+
+public class PacketOutPlayerPositionAndLook extends PacketOut {
+
+    private double x,y,z;
+    private float yaw, pitch;
+    private byte flags;
+
+    public PacketOutPlayerPositionAndLook(double x, double y, double z, float yaw, float pitch) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.yaw = yaw;
+        this.pitch = pitch;
+    }
+
+    @Override
+    public void serialize(ByteMessage byteMessage) {
+        byteMessage.writeDouble(x);
+        byteMessage.writeDouble(y);
+        byteMessage.writeDouble(z);
+        byteMessage.writeFloat(yaw);
+        byteMessage.writeFloat(pitch);
+        byteMessage.writeByte(0);
+    }
+
+
+}
