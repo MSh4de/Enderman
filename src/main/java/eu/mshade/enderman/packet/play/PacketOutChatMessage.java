b581e3a95cfce563c95b3fbcde0febda72b68ca8
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 20:13:16 +0200

    add PacketOutChatMessage 🎢

---
+++ src/main/java/eu/mshade/enderman/packet/play/PacketOutChatMessage.java
@@ -0,0 +1,23 @@
+package eu.mshade.enderman.packet.play;
+
+import eu.mshade.enderframe.mojang.chat.TextComponent;
+import eu.mshade.enderframe.mojang.chat.TextPosition;
+import eu.mshade.enderframe.protocol.ByteMessage;
+import eu.mshade.enderframe.protocol.PacketOut;
+
+public class PacketOutChatMessage extends PacketOut {
+
+    private TextComponent textComponent;
+    private TextPosition position;
+
+    public PacketOutChatMessage(TextComponent textComponent, TextPosition position) {
+        this.textComponent = textComponent;
+        this.position = position;
+    }
+
+    @Override
+    public void serialize(ByteMessage byteMessage) {
+        byteMessage.writeTextComponent(textComponent);
+        byteMessage.writeByte(position.getId());
+    }
+}
