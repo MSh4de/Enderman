52a2e6595c5d81c4e9271499861687a13927dc0a
NewZ_AZ <BenjaminDeveloppement@gmail.com>
2021-07-25 11:53:41 +0200

    Add EndermanByteMessage.java

--- src/main/java/eu/mshade/enderman/EndermanByteMessage.java
+++ src/main/java/eu/mshade/enderman/EndermanByteMessage.java
@@ -1,11 +1,21 @@
 package eu.mshade.enderman;
 
+import eu.mshade.enderframe.entity.EntityType;
+import eu.mshade.enderframe.metadata.MetadataEntry;
+import eu.mshade.enderframe.metadata.MetadataManager;
 import eu.mshade.enderframe.protocol.ByteMessage;
+import eu.mshade.enderman.metadata.EndermanMetadataManager;
 import io.netty.buffer.ByteBuf;
 
 public class EndermanByteMessage extends ByteMessage {
+    private static final MetadataManager metadataManager = new EndermanMetadataManager();
 
     public EndermanByteMessage(ByteBuf buf) {
         super(buf);
+    }
+
+    @Override
+    public void writeMetadata(EntityType entityType, MetadataEntry metadataEntry) {
+        metadataManager.write(this, entityType, metadataEntry);
     }
 }
