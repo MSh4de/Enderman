672f7c3bae53b1373d8dd709fdce363362ce9c8b
NewZ_AZ <BenjaminDeveloppement@gmail.com>
2021-08-07 14:05:28 +0200

    change method serialize to implement Entity

--- src/main/java/eu/mshade/enderman/packet/play/PacketOutEntityTeleport.java
+++ src/main/java/eu/mshade/enderman/packet/play/PacketOutEntityTeleport.java
@@ -1,32 +1,27 @@
 package eu.mshade.enderman.packet.play;
 
+import eu.mshade.enderframe.entity.Entity;
 import eu.mshade.enderframe.protocol.ByteMessage;
 import eu.mshade.enderframe.protocol.PacketOut;
 
 public class PacketOutEntityTeleport extends PacketOut {
-    private final int id;
-    private final int x,y,z;
-    private final byte yaw,pitch;
+    private final Entity entity;
     private final boolean onGround;
 
-    public PacketOutEntityTeleport(int id, int x, int y, int z, byte yaw, byte pitch, boolean onGround) {
-        this.id = id;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.yaw = yaw;
-        this.pitch = pitch;
+    public PacketOutEntityTeleport(Entity entity, boolean onGround) {
+        this.entity = entity;
         this.onGround = onGround;
     }
 
     @Override
     public void serialize(ByteMessage byteMessage) {
-        byteMessage.writeVarInt(id);
-        byteMessage.writeInt(x);
-        byteMessage.writeInt(y);
-        byteMessage.writeInt(z);
-        byteMessage.writeByte(yaw);
-        byteMessage.writeByte(pitch);
+        byteMessage.writeVarInt(entity.getEntityId());
+        byteMessage.writeInt((int) (entity.getLocation().getX() *32));
+        byteMessage.writeInt((int) (entity.getLocation().getY() *32));
+        byteMessage.writeInt((int) (entity.getLocation().getZ() *32));
+        byteMessage.writeByte((int) entity.getLocation().getYaw());
+        byteMessage.writeByte((int) entity.getLocation().getPitch());
         byteMessage.writeBoolean(onGround);
     }
+
 }
