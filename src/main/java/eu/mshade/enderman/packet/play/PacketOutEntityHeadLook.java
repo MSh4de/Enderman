c0c4825c0804f48a18c3d06695040b1cf5e67ddf
Zentae <yanistahiri560@gmail.com>
2021-08-31 01:46:55 +0200

    changed constructors parameters. 🏗

--- src/main/java/eu/mshade/enderman/packet/play/PacketOutEntityHeadLook.java
+++ src/main/java/eu/mshade/enderman/packet/play/PacketOutEntityHeadLook.java
@@ -1,21 +1,22 @@
 package eu.mshade.enderman.packet.play;
 
+import eu.mshade.enderframe.entity.Entity;
 import eu.mshade.enderframe.protocol.ByteMessage;
 import eu.mshade.enderframe.protocol.PacketOut;
 
 public class PacketOutEntityHeadLook extends PacketOut {
 
-    private final int id;
+    private final Entity entity;
     private final float rotation;
 
-    public PacketOutEntityHeadLook(int id, float rotation) {
-        this.id = id;
+    public PacketOutEntityHeadLook(Entity entity, float rotation) {
+        this.entity = entity;
         this.rotation = rotation;
     }
 
     @Override
     public void serialize(ByteMessage byteMessage) {
-        byteMessage.writeVarInt(id);
+        byteMessage.writeVarInt(entity.getEntityId());
         byteMessage.writeByte((int) rotation);
     }
 }
--- src/main/java/eu/mshade/enderman/packet/play/PacketOutEntityLookRelativeMove.java
+++ src/main/java/eu/mshade/enderman/packet/play/PacketOutEntityLookRelativeMove.java
@@ -1,18 +1,19 @@
 package eu.mshade.enderman.packet.play;
 
+import eu.mshade.enderframe.entity.Entity;
 import eu.mshade.enderframe.protocol.ByteMessage;
 import eu.mshade.enderframe.protocol.PacketOut;
 import eu.mshade.enderframe.world.Location;
 
 public class PacketOutEntityLookRelativeMove extends PacketOut  {
 
-    private final int id;
+    private final Entity entity;
     private final byte x,y,z;
     private final int yaw,pitch;
     private final boolean onGround;
 
-    public PacketOutEntityLookRelativeMove(int id, byte x, byte y, byte z, int yaw, int pitch, boolean onGround) {
-        this.id = id;
+    public PacketOutEntityLookRelativeMove(Entity entity, byte x, byte y, byte z, int yaw, int pitch, boolean onGround) {
+        this.entity = entity;
         this.x = x;
         this.y = y;
         this.z = z;
@@ -23,7 +24,7 @@ public class PacketOutEntityLookRelativeMove extends PacketOut  {
 
     @Override
     public void serialize(ByteMessage byteMessage) {
-        byteMessage.writeVarInt(id);
+        byteMessage.writeVarInt(entity.getEntityId());
         byteMessage.writeByte(x);
         byteMessage.writeByte(y);
         byteMessage.writeByte(z);
