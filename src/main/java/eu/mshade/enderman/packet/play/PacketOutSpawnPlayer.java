f6eb863ebd0fb4b5f2d810e704b83305400dd8ac
RealAlphaUA <realalpha.ua@gmail.com>
2021-11-07 10:20:53 +0100

    change calcul of yaw & pitch 🎪

--- src/main/java/eu/mshade/enderman/packet/play/PacketOutSpawnPlayer.java
+++ src/main/java/eu/mshade/enderman/packet/play/PacketOutSpawnPlayer.java
@@ -4,6 +4,8 @@ import eu.mshade.enderframe.entity.Player;
 import eu.mshade.enderframe.protocol.ByteMessage;
 import eu.mshade.enderframe.protocol.PacketOut;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class PacketOutSpawnPlayer extends PacketOut {
 
     private final Player player;
@@ -15,12 +17,12 @@ public class PacketOutSpawnPlayer extends PacketOut {
     @Override
     public void serialize(ByteMessage byteMessage) {
         byteMessage.writeVarInt(player.getEntityId());
-        byteMessage.writeUUID(player.getUUID());
+        byteMessage.writeUUID(player.getUniqueId());
         byteMessage.writeInt((int) player.getLocation().getX() * 32);
         byteMessage.writeInt((int) player.getLocation().getY() * 32);
         byteMessage.writeInt((int) player.getLocation().getZ() * 32);
-        byteMessage.writeByte((byte) (player.getLocation().getYaw() % 360 / 360 * 256));
-        byteMessage.writeByte((byte) (player.getLocation().getPitch() % 360 / 360 * 256));
+        byteMessage.writeByte((byte) (player.getLocation().getYaw() * 256 / 360));
+        byteMessage.writeByte((byte) (player.getLocation().getPitch() * 256 / 360));
         byteMessage.writeShort(0);
         byteMessage.writeByte(0x7F);
     }
