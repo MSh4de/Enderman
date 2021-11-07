b19dfb6ac875c2220cb164dc38be7f68eca2e972
RealAlphaUA <realalpha.ua@gmail.com>
2021-11-07 10:21:34 +0100

    change calcul of yam & pitch 🦺

--- src/main/java/eu/mshade/enderman/packet/play/PacketOutSpawnMob.java
+++ src/main/java/eu/mshade/enderman/packet/play/PacketOutSpawnMob.java
@@ -21,9 +21,9 @@ public class PacketOutSpawnMob extends PacketOut {
         byteMessage.writeInt(entity.getLocation().getBlockX() * 32);
         byteMessage.writeInt(entity.getLocation().getBlockY() * 32);
         byteMessage.writeInt(entity.getLocation().getBlockZ() * 32);
-        byteMessage.writeByte((byte) (entity.getLocation().getYaw() / 360 * 256));
-        byteMessage.writeByte((byte) (entity.getLocation().getPitch() / 360 * 256));
-        byteMessage.writeByte((byte) (entity.getLocation().getPitch() / 360 * 256));
+        byteMessage.writeByte((byte) (entity.getLocation().getYaw() * 256 / 360));
+        byteMessage.writeByte((byte) (entity.getLocation().getPitch() * 256 / 360));
+        byteMessage.writeByte((byte) 0);
         byteMessage.writeShort((int) entity.getVelocity().getX());
         byteMessage.writeShort((int) entity.getVelocity().getY());
         byteMessage.writeShort((int) entity.getVelocity().getZ());
