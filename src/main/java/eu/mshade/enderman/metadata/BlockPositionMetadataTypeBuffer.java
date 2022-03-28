353f84dc96d9b5c8e8f8b066a07a2c9ab4170834
RealAlphaUA <realalpha.ua@gmail.com>
2022-03-28 23:31:52 +0200

    using generic type 🎁

--- src/main/java/eu/mshade/enderman/metadata/BlockPositionMetadataTypeBuffer.java
+++ src/main/java/eu/mshade/enderman/metadata/BlockPositionMetadataTypeBuffer.java
@@ -1,18 +1,15 @@
 package eu.mshade.enderman.metadata;
 
-import eu.mshade.enderframe.metadata.Metadata;
-import eu.mshade.enderframe.metadata.MetadataType;
-import eu.mshade.enderframe.metadata.buffer.MetadataBuffer;
-import eu.mshade.enderframe.metadata.buffer.type.MetadataTypeBuffer;
+import eu.mshade.enderframe.metadata.MetadataBuffer;
 import eu.mshade.enderframe.metadata.type.BlockPositionMetadata;
 import eu.mshade.enderframe.protocol.ByteMessage;
 import eu.mshade.enderframe.world.BlockPosition;
 
-public class BlockPositionMetadataTypeBuffer implements MetadataTypeBuffer {
+public class BlockPositionMetadataTypeBuffer implements MetadataBuffer<BlockPositionMetadata> {
 
     @Override
-    public void write(ByteMessage byteMessage, Metadata<?> metadata) {
-        BlockPosition blockPosition = (BlockPosition) metadata.get();
+    public void write(ByteMessage byteMessage, BlockPositionMetadata blockPositionMetadata) {
+        BlockPosition blockPosition = blockPositionMetadata.get();
         byteMessage.writeInt(blockPosition.getX());
         byteMessage.writeInt(blockPosition.getY());
         byteMessage.writeInt(blockPosition.getZ());
