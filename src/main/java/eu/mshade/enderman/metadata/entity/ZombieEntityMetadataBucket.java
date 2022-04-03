d2f6ff4a747691583ea12fcef4167b8c4badd3dc
RealAlphaUA <realalpha.ua@gmail.com>
2022-04-04 01:27:49 +0200

    add ZombieEntityMetadataBucket 🎨

---
+++ src/main/java/eu/mshade/enderman/metadata/entity/ZombieEntityMetadataBucket.java
@@ -0,0 +1,19 @@
+package eu.mshade.enderman.metadata.entity;
+
+import eu.mshade.enderframe.metadata.EntityMetadataType;
+import eu.mshade.enderframe.metadata.type.ByteMetadata;
+
+public class ZombieEntityMetadataBucket extends LivingEntityEntityMetadataBucket {
+
+    public ZombieEntityMetadataBucket() {
+        this.registerEntityMetadata(12, entity -> new ByteMetadata(entity.<Boolean>getEntityMetadata(EntityMetadataType.CHILD).get()), EntityMetadataType.CHILD);
+        this.registerEntityMetadata(13, entity -> new ByteMetadata(entity.<Boolean>getEntityMetadata(EntityMetadataType.VILLAGER).get()), EntityMetadataType.VILLAGER);
+        this.registerEntityMetadata(14, entity -> new ByteMetadata(entity.<Boolean>getEntityMetadata(EntityMetadataType.CONVERTING).get()), EntityMetadataType.CONVERTING);
+        /*
+        getMetadataRepository().registerMetadataIndex(12, MetadataType.BYTE, EntityMetadataType.CHILD);
+        getMetadataRepository().registerMetadataIndex(13, MetadataType.BYTE, EntityMetadataType.IS_VILLAGER);
+        getMetadataRepository().registerMetadataIndex(14, MetadataType.BYTE, EntityMetadataType.IS_CONVERTING);
+
+         */
+    }
+}
