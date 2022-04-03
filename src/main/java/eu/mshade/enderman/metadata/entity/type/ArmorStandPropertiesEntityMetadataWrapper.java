f5aeb984b018af7cfcac0c1b82d732603a138529
RealAlphaUA <realalpha.ua@gmail.com>
2022-04-04 01:44:18 +0200

    add ArmorStandPropertiesEntityMetadataWrapper 🎨

---
+++ src/main/java/eu/mshade/enderman/metadata/entity/type/ArmorStandPropertiesEntityMetadataWrapper.java
@@ -0,0 +1,12 @@
+package eu.mshade.enderman.metadata.entity.type;
+
+import eu.mshade.enderframe.entity.ArmorStand;
+import eu.mshade.enderframe.metadata.EntityMetadataWrapper;
+import eu.mshade.enderframe.metadata.Metadata;
+
+public class ArmorStandPropertiesEntityMetadataWrapper implements EntityMetadataWrapper<ArmorStand> {
+    @Override
+    public Metadata<?> wrap(ArmorStand armorStand) {
+        return null;
+    }
+}
