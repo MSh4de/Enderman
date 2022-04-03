e2395e23febe0b3cf333241f3b04d9e4f1a4dc19
RealAlphaUA <realalpha.ua@gmail.com>
2022-04-04 01:44:49 +0200

    add SpiderEntityMetadataBucket 🎞

---
+++ src/main/java/eu/mshade/enderman/metadata/entity/SpiderEntityMetadataBucket.java
@@ -0,0 +1,11 @@
+package eu.mshade.enderman.metadata.entity;
+
+public class SpiderEntityMetadataBucket extends LivingEntityEntityMetadataBucket {
+
+    public SpiderEntityMetadataBucket() {
+        /*
+        this.getMetadataRepository().registerMetadataIndex(16, MetadataType.BYTE, EntityMetadataType.CLIMBING);
+
+         */
+    }
+}
