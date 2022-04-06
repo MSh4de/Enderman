93d864a34a75b1dd7f6ef97d18c541770a042d22
RealAlphaUA <realalpha.ua@gmail.com>
2022-04-07 00:06:39 +0200

    change name by EnderCrystalMetadataBucket 🎭

--- src/main/java/eu/mshade/enderman/metadata/entity/EnderCrystalMetadataBucket.java
+++ src/main/java/eu/mshade/enderman/metadata/entity/EnderCrystalMetadataBucket.java
@@ -3,9 +3,9 @@ package eu.mshade.enderman.metadata.entity;
 import eu.mshade.enderframe.metadata.EntityMetadataType;
 import eu.mshade.enderframe.metadata.type.IntegerMetadata;
 
-public class EnderCrystalEntityMetadataBucket extends EntityEntityMetadataBucket {
+public class EnderCrystalMetadataBucket extends DefaultEntityMetadataBucket {
 
-    public EnderCrystalEntityMetadataBucket() {
+    public EnderCrystalMetadataBucket() {
         this.registerEntityMetadata(8, entity -> new IntegerMetadata(entity.<Float>getEntityMetadata(EntityMetadataType.HEALTH).get().intValue()), EntityMetadataType.HEALTH);
         /*
         this.getMetadataRepository().registerMetadataIndex(8, MetadataType.INTEGER, EntityMetadataType.HEALTH);
