e87bc6ffa080e43aaa511069eaa2e1fb3ba94e9b
RealAlphaUA <realalpha.ua@gmail.com>
2022-04-07 00:07:11 +0200

    change name by DefaultEntityMetadataBucket 🥽

--- src/main/java/eu/mshade/enderman/metadata/entity/DefaultEntityMetadataBucket.java
+++ src/main/java/eu/mshade/enderman/metadata/entity/DefaultEntityMetadataBucket.java
@@ -5,13 +5,13 @@ import eu.mshade.enderframe.metadata.EntityMetadataType;
 import eu.mshade.enderframe.metadata.type.ByteMetadata;
 import eu.mshade.enderframe.metadata.type.ShortMetadata;
 import eu.mshade.enderframe.metadata.type.StringMetadata;
-import eu.mshade.enderman.metadata.entity.type.EntityPropertiesEntityMetadataWrapper;
+import eu.mshade.enderman.metadata.entity.type.EntityPropertiesMetadataWrapper;
 
 
-public class EntityEntityMetadataBucket extends EntityMetadataBucket {
+public class DefaultEntityMetadataBucket extends EntityMetadataBucket {
 
-    public EntityEntityMetadataBucket() {
-        this.registerEntityMetadata(0, new EntityPropertiesEntityMetadataWrapper(), EntityMetadataType.ON_FIRE, EntityMetadataType.CROUCHED, EntityMetadataType.SPRINTING, EntityMetadataType.HANDLING, EntityMetadataType.INVISIBLE);
+    public DefaultEntityMetadataBucket() {
+        this.registerEntityMetadata(0, new EntityPropertiesMetadataWrapper(), EntityMetadataType.ON_FIRE, EntityMetadataType.CROUCHED, EntityMetadataType.SPRINTING, EntityMetadataType.HANDLING, EntityMetadataType.INVISIBLE);
         this.registerEntityMetadata(1, entity -> new ShortMetadata(entity.<Short>getEntityMetadata(EntityMetadataType.AIR_TICKS).get()), EntityMetadataType.AIR_TICKS);
         this.registerEntityMetadata(2, entity -> new StringMetadata(entity.<String>getEntityMetadata(EntityMetadataType.CUSTOM_NAME).get()), EntityMetadataType.CUSTOM_NAME);
         this.registerEntityMetadata(3, entity -> new ByteMetadata(entity.<Boolean>getEntityMetadata(EntityMetadataType.CUSTOM_NAME_VISIBLE).get()), EntityMetadataType.CUSTOM_NAME_VISIBLE);
