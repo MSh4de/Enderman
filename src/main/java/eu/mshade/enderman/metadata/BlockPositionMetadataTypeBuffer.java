eeffc9e40cbda779614eb218d801f89e52e49d4b
RealAlphaUA <realalpha.ua@gmail.com>
2022-04-12 23:48:38 +0200

    using ByteBuf 🎪

--- src/main/java/eu/mshade/enderman/metadata/BlockPositionMetadataTypeBuffer.java
+++ src/main/java/eu/mshade/enderman/metadata/BlockPositionMetadataTypeBuffer.java
@@ -4,14 +4,15 @@ import eu.mshade.enderframe.metadata.MetadataBuffer;
 import eu.mshade.enderframe.metadata.type.BlockPositionMetadata;
 import eu.mshade.enderframe.protocol.ByteMessage;
 import eu.mshade.enderframe.world.BlockPosition;
+import io.netty.buffer.ByteBuf;
 
 public class BlockPositionMetadataTypeBuffer implements MetadataBuffer<BlockPositionMetadata> {
 
     @Override
-    public void write(ByteMessage byteMessage, BlockPositionMetadata blockPositionMetadata) {
+    public void write(ByteBuf byteBuf, BlockPositionMetadata blockPositionMetadata) {
         BlockPosition blockPosition = blockPositionMetadata.get();
-        byteMessage.writeInt(blockPosition.getX());
-        byteMessage.writeInt(blockPosition.getY());
-        byteMessage.writeInt(blockPosition.getZ());
+        byteBuf.writeInt(blockPosition.getX());
+        byteBuf.writeInt(blockPosition.getY());
+        byteBuf.writeInt(blockPosition.getZ());
     }
 }
