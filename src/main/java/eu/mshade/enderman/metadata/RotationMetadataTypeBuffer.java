fd1014e472d9aa1ce6b44f69a373d9e37d2d9c51
RealAlphaUA <realalpha.ua@gmail.com>
2022-04-12 23:48:29 +0200

    using ByteBuf 🎪

--- src/main/java/eu/mshade/enderman/metadata/RotationMetadataTypeBuffer.java
+++ src/main/java/eu/mshade/enderman/metadata/RotationMetadataTypeBuffer.java
@@ -4,13 +4,14 @@ import eu.mshade.enderframe.metadata.MetadataBuffer;
 import eu.mshade.enderframe.metadata.type.RotationMetadata;
 import eu.mshade.enderframe.protocol.ByteMessage;
 import eu.mshade.enderframe.world.Rotation;
+import io.netty.buffer.ByteBuf;
 
 public class RotationMetadataTypeBuffer implements MetadataBuffer<RotationMetadata> {
     @Override
-    public void write(ByteMessage byteMessage, RotationMetadata rotationMetadata) {
+    public void write(ByteBuf byteBuf, RotationMetadata rotationMetadata) {
         Rotation rotation = rotationMetadata.get();
-        byteMessage.writeFloat(rotation.getYaw());
-        byteMessage.writeFloat(rotation.getPitch());
-        byteMessage.writeFloat(rotation.getRoll());
+        byteBuf.writeFloat(rotation.getYaw());
+        byteBuf.writeFloat(rotation.getPitch());
+        byteBuf.writeFloat(rotation.getRoll());
     }
 }
