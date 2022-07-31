c9fcd4e3430f140a38a70909f2c1f8a76f1f64c0
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-01 00:29:46 +0200

    add EndermanInventorySizeWrapper 🎠

---
+++ src/main/java/eu/mshade/enderman/wrapper/EndermanInventorySizeWrapper.java
@@ -0,0 +1,15 @@
+package eu.mshade.enderman.wrapper;
+
+import eu.mshade.enderframe.inventory.InventoryKey;
+import eu.mshade.enderframe.inventory.InventoryType;
+import eu.mshade.enderframe.wrapper.Wrapper;
+
+public class EndermanInventorySizeWrapper extends Wrapper<InventoryKey, Integer> {
+
+    public EndermanInventorySizeWrapper() {
+        this.registerInput(InventoryType.WORKBENCH, 9);
+        this.registerInput(InventoryType.CHEST, 9*3);
+        this.registerInput(InventoryType.HOPPER, 5);
+        this.registerInput(InventoryType.PLAYER, 45);
+    }
+}
