337f3819dff9e262634b871a5b81f53b2842bde2
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-05 20:18:53 +0200

    modify test code 🧵

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,7 @@
 import eu.mshade.enderframe.item.ItemFlag;
 import eu.mshade.enderframe.item.Material;
 import eu.mshade.enderframe.item.MaterialKey;
+import eu.mshade.enderframe.world.Vector;
 import eu.mshade.enderman.wrapper.EndermanMaterialWrapper;
 
 import java.util.List;
@@ -10,13 +11,6 @@ public class Test {
 
     public Test() {
 
-
-        EndermanMaterialWrapper endermanMaterialWrapper = new EndermanMaterialWrapper();
-        System.out.println(endermanMaterialWrapper.reverse(MaterialKey.from(2)));
-        System.out.println(endermanMaterialWrapper.wrap(Material.GRASS));
-
-        System.out.println(ItemFlag.toByte(List.of(ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_DESTROYS)));
-        System.out.println(ItemFlag.fromByte(12));
     }
 
     public static void main(String[] args) {
