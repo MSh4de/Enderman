f87e45f8cec58c43ecfee35bd597389fa4958f14
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-30 23:15:14 +0200

    add EndermanContextWrapper 🥽

---
+++ src/main/java/eu/mshade/enderman/wrapper/EndermanContextWrapper.java
@@ -0,0 +1,16 @@
+package eu.mshade.enderman.wrapper;
+
+import eu.mshade.enderframe.wrapper.ContextWrapper;
+import eu.mshade.enderframe.wrapper.ContextWrapperKey;
+
+public class EndermanContextWrapper {
+
+    public static ContextWrapperKey INVENTORY_KEY = ContextWrapperKey.Companion.from("INVENTORY_KEY");
+    public static ContextWrapperKey INVENTORY_SIZE = ContextWrapperKey.Companion.from("INVENTORY_SIZE");
+    public static ContextWrapperKey ATTRIBUTE_KEY = ContextWrapperKey.Companion.from("ATTRIBUTE_KEY");
+    public static ContextWrapperKey ENCHANTMENT_TYPE = ContextWrapperKey.Companion.from("ENCHANTMENT_TYPE");
+    public static ContextWrapperKey EQUIPMENT_SLOT = ContextWrapperKey.Companion.from("EQUIPMENT_SLOT");
+    public static ContextWrapperKey ENTITY_TYPE = ContextWrapperKey.Companion.from("ENTITY_TYPE");
+    public static ContextWrapperKey NAMESPACED_KEY = ContextWrapperKey.Companion.from("NAMESPACED_KEY");
+
+}
