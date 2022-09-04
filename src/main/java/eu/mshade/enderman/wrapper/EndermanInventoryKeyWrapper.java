df9369779b68d54582032fd3dd39439a6deca1cb
RealAlphaUA <realalpha.ua@gmail.com>
2022-09-04 23:59:33 +0200

    add other wrap for inventoryType 🛒

--- src/main/java/eu/mshade/enderman/wrapper/EndermanInventoryKeyWrapper.java
+++ src/main/java/eu/mshade/enderman/wrapper/EndermanInventoryKeyWrapper.java
@@ -10,6 +10,15 @@ public class EndermanInventoryKeyWrapper extends Wrapper<InventoryKey, String> {
     public EndermanInventoryKeyWrapper() {
         this.register(InventoryType.CHEST, NamespacedKey.minecraft("chest").toString());
         this.register(InventoryType.HOPPER, NamespacedKey.minecraft("hopper").toString());
+        this.register(InventoryType.FURNACE, NamespacedKey.minecraft("furnace").toString());
+        this.register(InventoryType.WORKBENCH, NamespacedKey.minecraft("crafting_table").toString());
+        this.register(InventoryType.DISPENSER, NamespacedKey.minecraft("dispenser").toString());
+        this.register(InventoryType.ENCHANTMENT_TABLE, NamespacedKey.minecraft("enchanting_table").toString());
+        this.register(InventoryType.BREWING_STAND, NamespacedKey.minecraft("brewing_stand").toString());
+        this.register(InventoryType.VILLAGER, NamespacedKey.minecraft("villager").toString());
+        this.register(InventoryType.BEACON, NamespacedKey.minecraft("beacon").toString());
+
+
 
     }
 }
