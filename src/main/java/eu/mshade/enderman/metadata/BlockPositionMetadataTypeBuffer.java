937b3be555662f3764c873f694faba45b5da6392
RealAlphaUA <realalpha.ua@gmail.com>
2022-10-18 22:00:42 +0200

    use Vector object 🎨

--- src/main/java/eu/mshade/enderman/metadata/BlockPositionMetadataTypeBuffer.java
+++ src/main/java/eu/mshade/enderman/metadata/BlockPositionMetadataTypeBuffer.java
@@ -3,15 +3,15 @@ package eu.mshade.enderman.metadata;
 import eu.mshade.enderframe.metadata.MetadataBuffer;
 import eu.mshade.enderframe.metadata.type.BlockPositionMetadata;
 import eu.mshade.enderframe.protocol.ProtocolBuffer;
-import eu.mshade.enderframe.world.BlockPosition;
+import eu.mshade.enderframe.world.Vector;
 
 public class BlockPositionMetadataTypeBuffer implements MetadataBuffer<BlockPositionMetadata> {
 
     @Override
     public void write(ProtocolBuffer protocolBuffer, BlockPositionMetadata blockPositionMetadata) {
-        BlockPosition blockPosition = blockPositionMetadata.get();
-        protocolBuffer.writeInt(blockPosition.getX());
-        protocolBuffer.writeInt(blockPosition.getY());
-        protocolBuffer.writeInt(blockPosition.getZ());
+        Vector blockPosition = blockPositionMetadata.get();
+        protocolBuffer.writeInt(blockPosition.getBlockX());
+        protocolBuffer.writeInt(blockPosition.getBlockY());
+        protocolBuffer.writeInt(blockPosition.getBlockZ());
     }
 }
