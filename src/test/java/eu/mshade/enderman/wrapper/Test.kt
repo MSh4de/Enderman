8dd26a39d997056d0c1dd66ca59b76eb86383307
RealAlphaUA <realalpha.ua@gmail.com>
2022-10-28 23:14:37 +0200

    update ✨

--- src/test/java/eu/mshade/enderman/wrapper/Test.kt
+++ src/test/java/eu/mshade/enderman/wrapper/Test.kt
@@ -5,17 +5,10 @@ import eu.mshade.enderframe.item.MaterialKey
 import eu.mshade.enderframe.wrapper.ContextWrapper
 import eu.mshade.enderframe.wrapper.Wrapper
 import eu.mshade.enderframe.wrapper.WrapperRepository
+import java.util.*
 
 fun main() {
-    val wrapperRepository = WrapperRepository()
-    val wrapper = Wrapper<MaterialKey, MaterialKey>()
-    wrapper.register(Material.STONE, MaterialKey.from(76))
 
-    wrapperRepository.register(ContextWrapper.MATERIAL_KEY, wrapper)
-
-    val materialKeyWrapper: Wrapper<MaterialKey, MaterialKey>? =
-        wrapperRepository.get(ContextWrapper.MATERIAL_KEY) as? Wrapper<MaterialKey, MaterialKey>
-    println(materialKeyWrapper?.wrap(Material.STONE))
 
 
 }
