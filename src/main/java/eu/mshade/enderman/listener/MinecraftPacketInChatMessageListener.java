d273f2d5e246fca0740bde8defc43ac50a1dec0f
RealAlphaUA <realalpha.ua@gmail.com>
2022-12-21 00:16:55 +0100

    add MinecraftPacketInChatMessageListener 🥏

---
+++ src/main/java/eu/mshade/enderman/listener/MinecraftPacketInChatMessageListener.java
@@ -0,0 +1,15 @@
+package eu.mshade.enderman.listener;
+
+import eu.mshade.enderframe.EnderFrame;
+import eu.mshade.enderframe.packetevent.MinecraftPacketChatMessageEvent;
+import eu.mshade.enderman.packet.play.MinecraftPacketInChatMessage;
+import eu.mshade.mwork.event.EventListener;
+
+public class MinecraftPacketInChatMessageListener implements EventListener<MinecraftPacketInChatMessage> {
+
+    @Override
+    public void onEvent(MinecraftPacketInChatMessage event) {
+        EnderFrame.get().getPacketEventBus().publishAsync(new MinecraftPacketChatMessageEvent(event.getMinecraftSession().getPlayer(), event.getMessage()));
+    }
+
+}
