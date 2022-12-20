2939c1e14c16459ed4db119062922c34d68ee2be
Azodox <likemoi99@gmail.com>
2022-12-20 21:41:53 +0100

    Implemented read method in FireworkExplosionItemStackMetadataBuffer :sparkles:

--- src/main/java/eu/mshade/enderman/metadata/itemstack/FireworkItemStackMetadataBuffer.kt
+++ src/main/java/eu/mshade/enderman/metadata/itemstack/FireworkItemStackMetadataBuffer.kt
@@ -1,10 +1,12 @@
 package eu.mshade.enderman.metadata.itemstack
 
 import eu.mshade.enderframe.item.FireworkExplosion
+import eu.mshade.enderframe.item.FireworkType
 import eu.mshade.enderframe.item.ItemStack
 import eu.mshade.enderframe.item.ItemStackMetadataBuffer
 import eu.mshade.enderframe.item.metadata.FireworkExplosionItemStackMetadata
 import eu.mshade.enderframe.metadata.itemstack.ItemStackMetadataKey
+import eu.mshade.enderframe.mojang.Color
 import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag
 
 class FireworkExplosionItemStackMetadataBuffer : ItemStackMetadataBuffer {
@@ -31,7 +33,32 @@ class FireworkExplosionItemStackMetadataBuffer : ItemStackMetadataBuffer {
         }
     }
 
-    override fun read(compoundBinaryTag: CompoundBinaryTag?, itemStack: ItemStack) {
-        TODO("Not yet implemented")
+    override fun read(compoundBinaryTag: CompoundBinaryTag, itemStack: ItemStack) {
+        val metadataKeyValueBucket = itemStack.metadataKeyValueBucket
+        if(!compoundBinaryTag.containsKey("Explosion")) return
+        val explosionTag = compoundBinaryTag.getBinaryTag("Explosion") as CompoundBinaryTag
+
+        if (!explosionTag.containsKey("Flicker")) return
+        val flicker = explosionTag.getByte("Flicker") == 1.toByte()
+
+        if (!explosionTag.containsKey("Trail")) return
+        val trail = explosionTag.getByte("Trail") == 1.toByte()
+
+        if (!explosionTag.containsKey("Type")) return
+        val type = FireworkType.getFireworkType(explosionTag.getByte("Type").toInt())
+
+        if (!explosionTag.containsKey("Colors")) return
+        val colorsArray = explosionTag.getIntArray("Colors")
+        val color = Color.fromRGB(colorsArray[0], colorsArray[1], colorsArray[2])
+
+        var fadeColor: Color? = null
+        if (explosionTag.containsKey("FadeColors")) {
+            val fadeColorsArray = explosionTag.getIntArray("FadeColors")
+            fadeColor = Color.fromRGB(fadeColorsArray[0], fadeColorsArray[1], fadeColorsArray[2])
+        }
+
+        FireworkExplosionItemStackMetadata(FireworkExplosion(flicker, trail, type, color, fadeColor)).apply {
+            metadataKeyValueBucket.setMetadataKeyValue(this)
+        }
     }
 }
