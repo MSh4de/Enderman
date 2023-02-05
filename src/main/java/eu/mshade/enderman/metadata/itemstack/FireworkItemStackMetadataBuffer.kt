62e0db5bcbdd7cac95b5e21d1b72e437e93d6241
RealAlphaUA <realalpha.ua@gmail.com>
2023-02-05 19:25:13 +0100

    merge itemstack to rework-use-mwork 🎨

--- src/main/java/eu/mshade/enderman/metadata/itemstack/FireworkItemStackMetadataBuffer.kt
+++ src/main/java/eu/mshade/enderman/metadata/itemstack/FireworkItemStackMetadataBuffer.kt
@@ -3,20 +3,18 @@ package eu.mshade.enderman.metadata.itemstack
 import eu.mshade.enderframe.item.FireworkExplosion
 import eu.mshade.enderframe.item.FireworkType
 import eu.mshade.enderframe.item.ItemStack
-import eu.mshade.enderframe.item.ItemStackMetadataBuffer
+import eu.mshade.enderframe.item.ItemStackMetadataWrapper
 import eu.mshade.enderframe.item.metadata.FireworkExplosionItemStackMetadata
 import eu.mshade.enderframe.metadata.itemstack.ItemStackMetadataKey
 import eu.mshade.enderframe.mojang.Color
 import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag
 
-class FireworkExplosionItemStackMetadataBuffer : ItemStackMetadataBuffer {
+class FireworkExplosionItemStackMetadataBuffer : ItemStackMetadataWrapper {
     override fun write(compoundBinaryTag: CompoundBinaryTag, itemStack: ItemStack) {
         val metadataKeyValueBucket = itemStack.metadataKeyValueBucket
-        val metadataKeyValue = metadataKeyValueBucket.getMetadataKeyValue(
-            ItemStackMetadataKey.EXPLOSION,
-            FireworkExplosionItemStackMetadata::class.java
-        )
+        val metadataKeyValue = metadataKeyValueBucket.getMetadataKeyValue(ItemStackMetadataKey.EXPLOSION) as FireworkExplosionItemStackMetadata
         val explosion: FireworkExplosion = metadataKeyValue.metadataValue
+
         val explosionTag = compoundBinaryTag.computeIfAbsent("Explosion") { CompoundBinaryTag() }
         explosionTag.putByte("Flicker", explosion.flicker)
         explosionTag.putByte("Trail", explosion.trail)
--- src/main/java/eu/mshade/enderman/metadata/window/EndermanWindowPropertyManager.java
+++
@@ -1,6 +0,0 @@
-package eu.mshade.enderman.metadata.window;
-
-public class EndermanWindowPropertyManager {
-
-
-}
