d94479bac9164929cc172bf6462e6694a79c6b4f
ilian <tokydevfr@gmail.com>
2023-05-07 22:28:21 +0200

    Change the param Entity to Player ⚙️

--- src/main/java/eu/mshade/enderman/packet/play/animation/MinecraftPacketOutAnimation.kt
+++ src/main/java/eu/mshade/enderman/packet/play/animation/MinecraftPacketOutAnimation.kt
@@ -1,13 +1,14 @@
 package eu.mshade.enderman.packet.play.animation
 
-import eu.mshade.enderframe.entity.Entity
+import eu.mshade.enderframe.animation.AnimationType
+import eu.mshade.enderframe.entity.Player
 import eu.mshade.enderframe.protocol.MinecraftByteBuf
 import eu.mshade.enderframe.protocol.MinecraftPacketOut
 
-class MinecraftPacketOutAnimation(private val entity: Entity, private val animationType: AnimationType) : MinecraftPacketOut {
+class MinecraftPacketOutAnimation(private val player: Player, private val animationType: AnimationType) : MinecraftPacketOut {
 
     override fun serialize(minecraftByteBuf: MinecraftByteBuf) {
-        minecraftByteBuf.writeVarInt(entity.getEntityId())
+        minecraftByteBuf.writeVarInt(player.getEntityId())
         minecraftByteBuf.writeVarInt(animationType.value)
     }
 }
